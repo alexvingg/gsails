f2b8f89e922220643a338c701aebd5a57a4e5709
Alex <alexvingg@gmail.com>
2016-11-03 11:46:12 -0200

    criando api

---
+++ api/controllers/AnalistaController.js
@@ -0,0 +1,11 @@
+/**
+ * AnalistaController
+ *
+ * @description :: Server-side logic for managing analistas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
